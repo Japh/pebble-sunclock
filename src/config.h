196967d9a3466855e9ff857fe2f42fdfd1ff1142
Japh <japhie@gmail.com>
2013-04-18 18:44:48 +1000

    Updated latitude, longitude, and timezone for Hobart, Australia

--- src/config.h
+++ src/config.h
@@ -1,4 +1,4 @@
 //NOTE: longitude is positive for East and negative for West
-#define LATITUDE    37.3231
-#define LONGITUDE -122.0311
-#define TIMEZONE -8;
+#define LATITUDE   -42.8806
+#define LONGITUDE -147.3250
+#define TIMEZONE 10;
